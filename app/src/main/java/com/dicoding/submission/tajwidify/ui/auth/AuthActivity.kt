a99706cb836b6cc56a5066306ffb380ff890566f
aafif <aafifpratama@gmail.com>
2023-12-14 10:17:10 +0700

    [style] change a logo apps

--- app/src/main/java/com/dicoding/submission/tajwidify/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/dicoding/submission/tajwidify/ui/auth/AuthActivity.kt
@@ -1,9 +1,10 @@
-package com.dicoding.submission.tajwidify
+package com.dicoding.submission.tajwidify.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.submission.tajwidify.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
