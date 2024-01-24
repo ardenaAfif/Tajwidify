00d0cc42c93ce25ece704f180ef4127f5151fa42
aafif <aafifpratama@gmail.com>
2024-01-24 07:01:40 +0700

    Fixing bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kuliah.pkm.tajwidify"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
