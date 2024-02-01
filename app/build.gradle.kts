8c4774ca5877f47f40ec10fe744dcd76d79729d8
aafif <aafifpratama@gmail.com>
2024-02-01 19:28:40 +0700

    [Update] Update version code to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kuliah.pkm.tajwidify"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
